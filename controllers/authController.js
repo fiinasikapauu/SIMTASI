5146c3fb052e547f9913ba00b24a411e95d4230b
fiinasikapauu <2311523041_rafi@student.unand.ac.id>
2025-06-20 05:37:50 +0700

    fix: proposal, revisi laporan, laporan kemajuan, feedback proposal dan laporan kemajuan

--- controllers/authController.js
+++ controllers/authController.js
@@ -115,3 +115,12 @@ exports.login = async (req, res) => {
     `);
   }
 };
+
+exports.logout = (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      return res.status(500).send('Gagal logout');
+    }
+    return res.redirect('/signin');
+  });
+};
